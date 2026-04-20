1592e211f7722edb183bfebbe7d3e2b30fe5d673
Alessiopopino <98519958+Alessiopopino@users.noreply.github.com>
2026-04-20 04:46:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const scrollToTopBtn = document.getElementById("scroll-to-top");
 
   // ===== MODALITÀ MANUTENZIONE =====
-  const MAINTENANCE_MODE = true ; // Cambia in true per attivare la manutenzione
+  const MAINTENANCE_MODE = false ; // Cambia in true per attivare la manutenzione
 
   // ===== FUNZIONE ESCAPE XSS =====
   function escapeHtml(str) {
